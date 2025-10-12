a22bbeed60ae7fc1c48e10daf3df39bed129b29e
abror <abror@MacBook-Pro-2.local>
2025-10-12 17:49:09 +0500

    done

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -15,17 +15,21 @@ public class CarServiceImpl implements CarService {
             new Car("Ferrari", 750, 380),
             new Car("Mazda", 250, 200),
             new Car("Bugatti", 1200, 400),
+            new Car("Bugatti", 1200, 400),
+            new Car("Bugatti", 1200, 400),
+            new Car("Bugatti", 1200, 400),
+            new Car("Bugatti", 1200, 400),
+            new Car("Bugatti", 1200, 400),
+            new Car("Bugatti", 1200, 400),
             new Car("Opel", 150, 180)
     );
 
     @Override
     public List<Car> getCars(Integer count) {
-        int limit = (count == null || count <= 0 || count >= 5) ? 5 : count;
-        int k = Math.min(limit, cars.size());
-        return cars.subList(0, k);
+        int limit = (count == null || count <= 0) ? 0 : count;
+        if (limit >= cars.size()) {
+            limit = cars.size();
+        }
+        return cars.subList(0, limit);
     }
-
-
-
-
 }
